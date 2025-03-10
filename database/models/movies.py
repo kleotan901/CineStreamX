5427f8f889d07a10a09e0b9d6475819a8d3c04c3
Tetiana Kozakova <kleotan901@gmail.com>
2025-03-10 12:16:55 +0200

    Create database models for users, user profiles, and authentication tokens (activation, password reset, and refresh)

--- database/models/movies.py
+++ database/models/movies.py
@@ -1,9 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
-
-
+from database import Base
 
 class MovieModel(Base):
     __tablename__ = "movies"
